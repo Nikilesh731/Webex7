029baed4d88013b073e9bd23d41a33ca3f0f377a
Nikilesh731 <154057413+Nikilesh731@users.noreply.github.com>
2025-11-07 10:33:11 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import LoanEligibilityChecker from "./LoanEligibilityChecker";
+
+function App() {
+  return <LoanEligibilityChecker />;
+}
+
+export default App;  // This line is required!
